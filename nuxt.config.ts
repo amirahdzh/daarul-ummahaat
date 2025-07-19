16d9222d6f004e373877d130e45b97f3e17e1247
amirahdzh <amirahdzh@gmail.com>
2025-07-19 10:12:42 +0700

    fix: update apiBase URL to correct endpoint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://www.api.daarulummahaat.org/api'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://api.daarulummahaat.org/api'
     }
   }
 })
